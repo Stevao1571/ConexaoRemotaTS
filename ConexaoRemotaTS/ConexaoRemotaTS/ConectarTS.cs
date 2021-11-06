c13e5bdece7fa25ace298ae3454c8905fca84e38
Stevao1571 <64023285+Stevao1571@users.noreply.github.com>
2021-11-05 21:29:04 -0300

    Add files via upload

---
+++ ConexaoRemotaTS/ConexaoRemotaTS/ConectarTS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace ConexaoRemotaTS
+{
+    public class ConectarTS
+    {
+
+        public void ConnectarUsuario(string usuario, string senha)
+        {
+            DescobrirTS desc = new DescobrirTS();
+
+
+            Process ipconfigProcess = new Process();
+            ipconfigProcess.StartInfo.FileName = "cmdkey";
+            ipconfigProcess.StartInfo.Arguments = $"/generic:{desc.consultaservidor(usuario)} /user:Gondim\\{usuario} /pass:{senha}";
+            ipconfigProcess.StartInfo.RedirectStandardOutput = true;
+            ipconfigProcess.StartInfo.UseShellExecute = false;
+            ipconfigProcess.Start();
+            ipconfigProcess.StartInfo.FileName = "mstsc";
+            ipconfigProcess.StartInfo.Arguments = $@"/v: {desc.consultaservidor(usuario)} ";
+            //ipconfigProcess.StartInfo.RedirectStandardOutput = true;
+            //ipconfigProcess.StartInfo.UseShellExecute = false;
+            ipconfigProcess.Start();
+            StreamReader reader = ipconfigProcess.StandardOutput;
+
+        }
+
+
+
+        public class UserInfo
+        {
+            public string Domain { get; set; }
+            public string User { get; set; }
+
+
+
+        }
+
+    }
+}
---
+++ ConexaoRemotaTS/ConexaoRemotaTS/DescobrirTS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.DirectoryServices.AccountManagement;
+
+namespace ConexaoRemotaTS
+{
+     public class DescobrirTS
+    {
+
+        public string consultaservidor(string usuario)
+        {
+            if (usuario == null)
+            {
+                return "";
+            }
+            //PrincipalContext yourDomain = new PrincipalContext(ContextType.Domain);            
+            PrincipalContext yourDomain = new PrincipalContext(ContextType.Domain);
+            // find the user
+            UserPrincipal user = UserPrincipal.FindByIdentity(yourDomain, usuario);
+            if (user == null)
+            {
+                return "";
+            }
+            else
+            {
+                return user.Description;
+            }
+        }
+    }
+}
